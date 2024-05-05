d399e60265cf71c71bae83f98716b5ee9f4ae9b0
PatraSuchi <trading.suchibrata.com@gmail.com>
2024-05-06 01:12:59 +0530

    update

--- verify.php
+++ verify.php
@@ -32,7 +32,7 @@ if (isset($_POST['email_code'])) {
 <!DOCTYPE html>
 <html lang="en">
  
-<head> this
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
