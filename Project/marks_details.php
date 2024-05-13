773bc0f01afd8d205c471d6df769326e88e3f71e
PatraSuchi <trading.suchibrata.com@gmail.com>
2024-05-13 23:24:49 +0530

    Update

--- Project/marks_details.php
+++ Project/marks_details.php
@@ -25,6 +25,12 @@ require 'session.php';
       integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
       crossorigin="anonymous"
     />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
     <style>
       body {
         font-family: Arial, sans-serif;
@@ -116,14 +122,315 @@ require 'session.php';
               </ul>
             </div>
 
-  <!--- This is the beginning of the Card Body portion-->
+            <!--- This is the beginning of the Card Body portion-->
+
             <div class="card-body">
-              
+              <form method="post" action="marks_details.php">
+                <!-- Start of the Marks Entering Details -->
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputCity" style="display: flex"
+                      >Subjects</label
+                    >
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      value="Bengali"
+                      disabled
+                    />
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="inputState" style="display: flex"
+                      >Obtained Marks</label
+                    >
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      placeholder=" Enter Your Marks "
+                    />
+                  </div>
+                  <div class="form-group col-md-2">
+                    <label for="inputZip" style="display: flex"
+                      >Full Marks</label
+                    >
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputZip"
+                      value="100"
+                    />
+                  </div>
+                </div>
+                <!-- End of the Marks Entering Page -->
+
+                <!-- Start of the 2nd Marks Entering Details -->
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <!-- <label for="inputCity" style="display: flex">Subjects</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      value="English"
+                      disabled
+                    />
+                  </div>
+                  <div class="form-group col-md-4">
+                    <!-- <label for="inputState" style="display: flex">Obtained Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      placeholder=" Enter Your Marks "
+                    />
+                  </div>
+                  <div class="form-group col-md-2">
+                    <!-- <label for="inputZip" style="display: flex" >Full Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputZip"
+                      value="100"
+                    />
+                  </div>
+                </div>
+                <!-- End of the 2nd Marks Entering Page -->
+
+                <!-- Start of the 3rd Marks Entering Details -->
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <!-- <label for="inputCity" style="display: flex">Subjects</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      value="Mathematics"
+                      disabled
+                    />
+                  </div>
+                  <div class="form-group col-md-4">
+                    <!-- <label for="inputState" style="display: flex">Obtained Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      placeholder=" Enter Your Marks "
+                    />
+                  </div>
+                  <div class="form-group col-md-2">
+                    <!-- <label for="inputZip" style="display: flex" >Full Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputZip"
+                      value="100"
+                    />
+                  </div>
+                </div>
+                <!-- End of the 3rd Marks Entering Page -->
+
+                <!-- Start of the 4th Marks Entering Details -->
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <!-- <label for="inputCity" style="display: flex">Subjects</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      value="Physical Science"
+                      disabled
+                    />
+                  </div>
+                  <div class="form-group col-md-4">
+                    <!-- <label for="inputState" style="display: flex">Obtained Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      placeholder=" Enter Your Marks "
+                    />
+                  </div>
+                  <div class="form-group col-md-2">
+                    <!-- <label for="inputZip" style="display: flex" >Full Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputZip"
+                      value="100"
+                    />
+                  </div>
+                </div>
+                <!-- End of the 4th Marks Entering Page -->
+
+                <!-- Start of the 5th Marks Entering Details -->
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <!-- <label for="inputCity" style="display: flex">Subjects</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      value="Life Science"
+                      disabled
+                    />
+                  </div>
+                  <div class="form-group col-md-4">
+                    <!-- <label for="inputState" style="display: flex">Obtained Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      placeholder=" Enter Your Marks "
+                    />
+                  </div>
+                  <div class="form-group col-md-2">
+                    <!-- <label for="inputZip" style="display: flex" >Full Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputZip"
+                      value="100"
+                    />
+                  </div>
+                </div>
+                <!-- End of the 5th Marks Entering Page -->
+
+                <!-- Start of the 6th Marks Entering Details -->
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <!-- <label for="inputCity" style="display: flex">Subjects</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      value="History"
+                      disabled
+                    />
+                  </div>
+                  <div class="form-group col-md-4">
+                    <!-- <label for="inputState" style="display: flex">Obtained Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      placeholder=" Enter Your Marks "
+                    />
+                  </div>
+                  <div class="form-group col-md-2">
+                    <!-- <label for="inputZip" style="display: flex" >Full Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputZip"
+                      value="100"
+                    />
+                  </div>
+                </div>
+                <!-- End of the 6th Marks Entering Page -->
+
+                <!-- Start of the 6th Marks Entering Details -->
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <!-- <label for="inputCity" style="display: flex">Subjects</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      value="Geography"
+                      disabled
+                    />
+                  </div>
+                  <div class="form-group col-md-4">
+                    <!-- <label for="inputState" style="display: flex">Obtained Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      placeholder=" Enter Your Marks "
+                    />
+                  </div>
+                  <div class="form-group col-md-2">
+                    <!-- <label for="inputZip" style="display: flex" >Full Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputZip"
+                      value="100"
+                    />
+                  </div>
+                </div>
+                <!-- End of the 6th Marks Entering Page -->
+
+                <hr />
+                <!-- Start of the 6th Marks Entering Details -->
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <!-- <label for="inputCity" style="display: flex">Subjects</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      value="Total Marks and "
+                      disabled
+                    />
+                  </div>
+                  <div class="form-group col-md-4">
+                    <!-- <label for="inputState" style="display: flex">Obtained Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputCity"
+                      placeholder=" Enter Your Marks "
+                    />
+                  </div>
+                  <div class="form-group col-md-2">
+                    <!-- <label for="inputZip" style="display: flex" >Full Marks</label> -->
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="inputZip"
+                      value="100"
+                    />
+                  </div>
+                </div>
+                <!-- End of the 6th Marks Entering Page -->
+                <button type="submit" name="reg_user" class="btn btn-primary">
+                  Save
+                </button>
+              </form>
             </div>
-            <div style="margin-left: 60%; padding-bottom: 2%;">
-              <a href="welcome.php" style="color: black;text-decoration: none;"> <button type="button" class="btn btn-primary" style="margin-right: 2%; background-color: rgb(255, 255, 255);color: black;"> Back</button></a>
-              <a href="welcome.php" style="color: black;text-decoration: none;"> <button type="button" class="btn btn-primary" style="margin-right: 2%; background-color: rgb(255, 255, 255);color: black;">Next</button></a>
-          </div>
+
+            <!-- This is the End of Card Body Portion-->
+            <div style="margin-left: 60%; padding-bottom: 2%">
+              <a href="welcome.php" style="color: black; text-decoration: none">
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  style="
+                    margin-right: 2%;
+                    background-color: rgb(255, 255, 255);
+                    color: black;
+                  "
+                >
+                  Back
+                </button>
+              </a>
+              <a href="welcome.php" style="color: black; text-decoration: none">
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  style="
+                    margin-right: 2%;
+                    background-color: rgb(255, 255, 255);
+                    color: black;
+                  "
+                >
+                  Next
+                </button></a
+              >
+            </div>
           </div>
         </div>
       </div>
