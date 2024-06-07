cf5bd513fe430f6933516dd281a0973931bea28b
PatraSuchi <trading.suchibrata.com@gmail.com>
2024-06-07 14:32:01 +0530

    Update

--- 9475755847/admission.php
+++ 9475755847/admission.php
@@ -108,9 +108,9 @@
         }
 
         .table thead th {
-            background-color: #f5f6f7;
+            background-color: #9BB0C1;
             border: none;
-            color: #6c757d;
+            color: #ffffff;
             font-weight: 600;
             padding: 15px;
             /* Increased padding */
@@ -118,7 +118,7 @@
 
         .table tbody tr {
             transition: background-color 0.3s ease;
-            background-color: #fff;
+            background-color: #FEFAF6;
         }
 
         .table tbody tr:hover {
@@ -160,6 +160,28 @@
             .section-subtitle {
                 margin-top: 20%;
             }
+        }
+
+        .list-group-item a {
+            color: rgb(227, 80, 80);
+            /* Bootstrap primary color */
+            text-decoration: none;
+        }
+
+        .list-group-item a:hover {
+            text-decoration: underline;
+        }
+
+        .list-group-item {
+            background-color: #f5f2e1;
+            /* Light grey background */
+            border-color: #d9d565;
+            /* Border color */
+        }
+
+        .list-group-item:nth-child(odd) {
+            background-color: #ece3bd;
+            /* Slightly darker grey for odd items */
         }
     </style>
 </head>
@@ -205,7 +227,7 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Important Dates</h5>
                                     <div class="table-responsive">
-                                        <table class="table">
+                                        <table class="table table-striped">
                                             <thead>
                                                 <tr>
                                                     <th scope="col">Procedures</th>
@@ -245,11 +267,11 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4 mb-4">
+                        <div class="col-md-4 mb-6">
                             <div class="card">
                                 <div class="card-body">
                                     <h5 class="card-title">
-                                        <center>Login</center>
+                                        <center>Applicant Login</center>
                                     </h5>
                                     <div class="table-responsive">
                                         <form method="post" action="login.php">
@@ -287,53 +309,176 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-8 mb-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title" style="padding-left: 3%;">Instructions</h5>
-                                <!-- <div class="table-responsive"> -->
-                                <p
-                                    style="text-align: justify; font-family: Arial, sans-serif; font-size: 16px; line-height: 1.6; margin: 20px;">
-                                <h3 style="font-size: 20px;">Required Documents</h3>
-                                <div class="container">
-                                    <table class="table" style="border-collapse: collapse; width: 100%;">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col" style="padding: 8px;">Document</th>
-                                                <th scope="col" style="padding: 8px;">Max Size</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td style="padding: 8px;  ">Passport Size Photo</td>
-                                                <td style="padding: 8px;  ">80 Kb</td>
-                                            </tr>
-                                            <tr>
-                                                <td style="padding: 8px;  ">Aadhar Card</td>
-                                                <td style="padding: 8px;  ">80 Kb</td>
-                                            </tr>
-                                            <tr>
-                                                <td style="padding: 8px;  ">Madhyamik Marksheet</td>
-                                                <td style="padding: 8px;  ">80 Kb</td>
-                                            </tr>
-                                            <tr>
-                                                <td style="padding: 8px;  ">Madhyamik Certificate</td>
-                                                <td style="padding: 8px;  ">80 Kb</td>
-                                            </tr>
-                                            <tr>
-                                                <td style="padding: 8px;  ">Personal Signature</td>
-                                                <td style="padding: 8px;  ">80 Kb</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                    
+                    <div class="row">
+                        <div class="col-md-8 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title" style="padding-left: 3%;">Instructions</h5>
+                                    <p
+                                        style="text-align: justify; font-family: Arial, sans-serif; font-size: 16px; line-height: 1.6; margin: 20px;">
+                                    <h3 style="font-size: 20px;">Required Documents</h3>
+                                    <div class="container">
+                                        <table class="table table-striped"
+                                            style="border-collapse: collapse; width: 100%;">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col" style="padding: 8px;">Document</th>
+                                                    <th scope="col" style="padding: 8px;">Max Size</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td style="padding: 8px;">Passport Size Photo</td>
+                                                    <td style="padding: 8px;">80 Kb</td>
+                                                </tr>
+                                                <tr>
+                                                    <td style="padding: 8px;">Aadhar Card</td>
+                                                    <td style="padding: 8px;">80 Kb</td>
+                                                </tr>
+                                                <tr>
+                                                    <td style="padding: 8px;">Madhyamik Marksheet</td>
+                                                    <td style="padding: 8px;">80 Kb</td>
+                                                </tr>
+                                                <tr>
+                                                    <td style="padding: 8px;">Madhyamik Certificate</td>
+                                                    <td style="padding: 8px;">80 Kb</td>
+                                                </tr>
+                                                <tr>
+                                                    <td style="padding: 8px;">Personal Signature</td>
+                                                    <td style="padding: 8px;">80 Kb</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    </p>
                                 </div>
-                                </p>
-                                <!-- </div> -->
+                            </div>
+                            <br>
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title" style="padding-left: 3%;">Contact Details</h5>
+                                    <div class="container">
+                                        <div class="table-responsive"> <!-- Added Bootstrap class for responsiveness -->
+                                            <table class="table table-striped"
+                                                style="border-collapse: collapse; width: 100%;">
+                                                <tbody>
+                                                    <tr>
+                                                        <td style="padding: 8px;"><ion-icon name="mail-outline"
+                                                                style="vertical-align: middle; display: inline-block;"></ion-icon>
+                                                            <b
+                                                                style="display: inline-block; vertical-align: middle;"></b>
+                                                        </td>
+                                                        <td style="padding: 8px;">Patra.group.official@gmail.com</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td style="padding: 8px;"><ion-icon name="call-outline"
+                                                                style="vertical-align: middle; display: inline-block;"></ion-icon>
+                                                            <b
+                                                                style="display: inline-block; vertical-align: middle;"></b>
+                                                        </td>
+                                                        <td style="padding: 8px;">+91 947575547</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td style="padding: 8px;"><ion-icon name="logo-whatsapp"
+                                                                style="vertical-align: middle; display: inline-block;"></ion-icon>
+                                                            <b
+                                                                style="display: inline-block; vertical-align: middle;"></b>
+                                                        </td>
+                                                        <td style="padding: 8px;">+91 8145302135</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
+                        <div class="col-md-4 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title" style="display: flex; align-items: center;">
+                                        <div
+                                            style="background-color: #EADFB4; border-radius: 3px;padding-top:7.3px;padding-bottom: 7.3px;padding-left: 6px;">
+                                            <ion-icon name="list-outline" style="margin-right: 8px;"></ion-icon>
+                                        </div>
+                                        <div style="margin-left: 5px;">Merit List</div>
+                                    </h5>
+                                    <?php
+                                    // Directory path
+                                    $directory = './HOI_signin/Merit_Lists/';
+            
+                                    // Scan the directory for files
+                                    $files = array_diff(scandir($directory), array('..', '.'));
+            
+                                    // Check if there are any files in the directory
+                                    if (count($files) > 0) {
+                                        $hasPdf = false;
+                                        $listCounter = 1;
+                                        echo '<ul class="list-group">';
+                                        foreach ($files as $file) {
+                                            if (pathinfo($file, PATHINFO_EXTENSION) == 'pdf') {
+                                                $hasPdf = true;
+                                                echo '<li class="list-group-item">
+                                                        <a href="' . $directory . $file . '">Merit List ' . $listCounter . '</a>
+                                                      </li>';
+                                                $listCounter++;
+                                            }
+                                        }
+                                        echo '</ul>';
+                                        if (!$hasPdf) {
+                                            echo '<p style="text-align: justify; font-family: Arial, sans-serif; font-size: 16px; line-height: 1.6; margin: 20px;">
+                                                    No List is Published Yet
+                                                  </p>';
+                                        }
+                                    } else {
+                                        echo '<p style="text-align: justify; font-family: Arial, sans-serif; font-size: 16px; line-height: 1.6; margin: 20px;">
+                                                No List is Published Yet
+                                              </p>';
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+
+                            <br>
+                            <div class="card"
+                                style="background-color: #ffffff; border: none; box-shadow: 0 4px 8px rgba(0,0,0,0.1); width: 100%; max-width: 400px; border-radius: 8px; overflow: hidden;">
+                                <div class="card-body" style="padding: 20px;">
+                                    <h3>Eligibility Criteria</h3>
+                                    <ul style="list-style-type: none; padding: 0;">
+                                        <li
+                                            style="background-color: #eff0f1; margin-bottom: 8px; padding: 10px; padding-left: 20px; border-radius: 5px; box-shadow: inset 0 0 5px rgba(0,0,0,0.03); position: relative;">
+                                            <span style="position: absolute; left: 10px;">•</span>You must pass all the
+                                            papers in the 10th level board exams.
+                                        </li>
+                                        <li
+                                            style="background-color: #eff0f1; margin-bottom: 8px; padding: 10px; padding-left: 20px; border-radius: 5px; box-shadow: inset 0 0 5px rgba(0,0,0,0.03); position: relative;">
+                                            <span style="position: absolute; left: 10px;">•</span> Successful completion
+                                            of the 10th grade board exams with passing grades in all subjects is a
+                                            prerequisite for admission to Class XI.
+                                        </li>
+                                        <li
+                                            style="background-color: #eff0f1; margin-bottom: 8px; padding: 10px; padding-left: 20px; border-radius: 5px; box-shadow: inset 0 0 5px rgba(0,0,0,0.03); position: relative;">
+                                            <span style="position: absolute; left: 10px;">•</span> Candidates should
+                                            maintain continuous academic engagement and should not have a gap in studies
+                                            exceeding two years prior to seeking admission.
+                                        </li>
+                                        <li
+                                            style="background-color: #eff0f1; margin-bottom: 8px; padding: 10px; padding-left: 20px; border-radius: 5px; box-shadow: inset 0 0 5px rgba(0,0,0,0.03); position: relative;">
+                                            <span style="position: absolute; left: 10px;">•</span> Prospective students
+                                            are expected to continue their formal education beyond the 10th grade in
+                                            order to be eligible for admission to Class XI.
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
+
+
                 </div>
+
                 </div>
             </section>
             <section class="section event" id="event" aria-label="event">
