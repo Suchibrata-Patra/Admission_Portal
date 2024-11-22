0e8f469319154ba6889d0722c2585063616277f3
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-11-22 20:14:47 +0530

    Changed Favicons

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include ('favicon.php') ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,28 +6,21 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php include ('header.php') ?>
-<?php include ('favicon.php') ?>
-  <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KSV7GPM5');</script>
-<!-- End Google Tag Manager -->
+
+
+ 
 
 
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TheApplication - Centralised Admission Portal</title>
-
+  <!-- <link rel="shortcut icon" href="/Assets/images/favicon.png" type="image/svg+xml"> -->
   <!-- favicon -->
   <!-- <link rel="shortcut icon" href="Assets/images/favicon.png" type="image/svg+xml"> -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;800&family=Poppins:wght@400;500;600;700&display=swap"
-    rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="/Assets/css/style_listing_page.css">
 </head>
 
