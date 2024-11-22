594efdb76474fd248686e098e6bfffed32e30839
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-11-23 04:30:54 +0530

    Update sorry.php

--- sorry.php
+++ sorry.php
@@ -158,7 +158,7 @@
     Powered By <br>
     <a href="/">
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/The_application.in_navbara_icon.png/220px-The_application.in_navbara_icon.png" 
-           alt="Site Icon" style="height:auto;width:30%;">
+           alt="Site Icon" style="height:auto;width:100%;">
     </a>
   </div>
 
