235b2f6839d02df45020c47aa1461f86c93cb899
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-11-22 16:43:26 +0530

    Modified

--- favicon.php
+++ favicon.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
-<link rel="shortcut icon" href="../Assets/images/favicon.png" type="image/svg+xml">
+    <link rel="shortcut icon" href="../Assets/images/favicon.png" type="image/svg+xml">
 </body>
+
 </html>
