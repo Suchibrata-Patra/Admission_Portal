5bc7035820b37a04d5586b16845c80d4d6d36d0f
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-11-22 16:42:09 +0530

    Modified

--- favicon.php
+++ favicon.php
@@ -1,1 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
 <link rel="shortcut icon" href="../Assets/images/favicon.png" type="image/svg+xml">
+</body>
+</html>
--- index.php
+++ index.php
@@ -16,7 +16,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>The Application - Centralised Admission Portal</title>
+  <title>TheApplication - Centralised Admission Portal</title>
 
   <!-- favicon -->
   <!-- <link rel="shortcut icon" href="Assets/images/favicon.png" type="image/svg+xml"> -->
