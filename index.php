e117d3a6dcca34fdebbbf66effd9ca769c782803
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-11-22 20:18:49 +0530

    Source Code Modified

--- index.php
+++ index.php
@@ -1,28 +1,18 @@
-<?php include ('favicon.php') ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php include ('header.php') ?>
-
-
- 
-
-
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="/Assets/images/favicon.png" type="image/svg+xml">
+  <?php include('header.php'); ?>
   <title>TheApplication - Centralised Admission Portal</title>
-  <!-- <link rel="shortcut icon" href="/Assets/images/favicon.png" type="image/svg+xml"> -->
-  <!-- favicon -->
-  <!-- <link rel="shortcut icon" href="Assets/images/favicon.png" type="image/svg+xml"> -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="/Assets/css/style_listing_page.css">
 </head>
+
 
 <body id="top">
 
