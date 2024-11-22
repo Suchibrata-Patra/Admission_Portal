b1f9e0b028c1be194fc66de54e1e27a3928178ba
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-11-22 20:45:02 +0530

    Modified

--- footer.php
+++ footer.php
@@ -1,141 +1,141 @@
-
 <style>
-   .footer-items{
+  .footer-items {
     color: rgb(255, 255, 255);
     font-size: 25px;
-   }
-  </style>
+  }
+</style>
 <section class="cta" id="contact">
-        <div class="container">
+  <div class="container">
 
-          <div class="cta-content">
-            <p class="section-subtitle">Connect With Us</p>
+    <div class="cta-content">
+      <p class="section-subtitle">Connect With Us</p>
 
-            <h3 class="h2 section-title">Any Issues Needs to be resolved Soon ?</h3>
+      <h3 class="h2 section-title">Any Issues Needs to be resolved Soon ?</h3>
 
-            <p class="section-text">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam eius obcaecati nulla suscipit temporibus
-              a mollitia deserunt eum rem iste!
-            </p>
-          </div>
-
-          <button class="btn btn-secondary">Contact Us !</button>
-
-        </div>
-      </section>
-  <meta name="description"
-    content="Welcome to the Class XII Admission Portal, your gateway to streamlined admissions in West Bengal. Discover top-rated higher secondary schools, simplify your application process, and secure your spot in the best institutions. Explore our user-friendly platform for hassle-free admissions today.">
-  <footer class="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="footer-brand">
-          <a href="#" class="logo">
-            <img src="../Assets/images/favicon.png" alt="Haggle logo" style="width: 15%;">
-          </a>
-          <p class="footer-text" description="Class XII Admission Portal">
-            Welcome to the Common Higher Secondary Admission Portal for students in West Bengal. Our platform simplifies
-            the application process for higher secondary schools across the state. Use our user-friendly search feature
-            to
-            find and apply to your preferred schools. Simply search for nearby schools and click 'Apply' to submit your
-            application. Our portal ensures a smooth and efficient admission experience, helping you secure a spot in
-            the
-            best higher secondary institutions in West Bengal.
-          </p>
-        </div>
-        <div class="footer-contact">
-          <h4 class="contact-title">Contact Us</h4>
-          <p class="contact-text">
-            Feel free to contact us for any inquiries or assistance.
-          </p>
-          <ul>
-            <li class="contact-item">
-              <ion-icon name="call-outline"></ion-icon>
-              <a href="tel:+919475755847" class="contact-link">+91 9475755847</a>
-            </li>
-            <li class="contact-item">
-              <ion-icon name="mail-outline"></ion-icon>
-              <a href="mailto:suchibratapatra2003@gmail.com" class="contact-link">suchibratapatra2003@gmail.com</a>
-            </li>
-            <li class="contact-item">
-              <ion-icon name="location-outline"></ion-icon>
-              <address>Diamond Harbour,West Bengal,INDIA</address>
-            </li>
-          </ul>
-          <br>
-          <ul>
-            <p>Quick Links</p>
-            <hr style="width: 30%; height: 2.5px; border: none; background-color:#3b79c9;">
-            <br>
-            <li class="contact-item">
-              <a href="index.php" class="contact-link">Home</a>
-            </li>
-            <li class="contact-item">
-              <a href="../../privacy_policy.php" class="contact-link">Privacy Policy</a>
-            </li>
-            <li class="contact-item">
-              <a href="../../Complete_List.php" class="contact-link">Search Results</a>
-            </li>
-            <li class="contact-item">
-              <a href="../../terms_and_conditions.php" class="contact-link">Terms & Conditions</a>
-            </li>
-            <li class="contact-item">
-              <a href="../../Digital-Fingerprint-Decoder.php" class="contact-link">Fingerprint Decoder</a>
-            </li>
-            <li class="contact-item">
-              <a href="../../password_guidelines.php" class="contact-link">Password Guidelines</a>
-            </li>
-          </ul>
-        </div>
-        <div class="footer-form">
-          <p class="form-text">
-            Subscribe to our newsletter for updates and news.
-          </p>
-          <form action="" class="form-wrapper">
-            <input type="email" name="email" class="input-field" placeholder="Enter Your Email" required>
-            <button type="submit" class="btn btn-secondary">Subscribe</button>
-          </form>
-        </div>
-      </div>
+      <p class="section-text">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam eius obcaecati nulla suscipit temporibus
+        a mollitia deserunt eum rem iste!
+      </p>
     </div>
-    <div class="footer-bottom">
-      <div class="container">
-        <p class="copyright">
-          &copy; 2024 <a href="">Theapplication.in</a> All rights reserved
-        </p>
-        <ul class="footer-bottom-list">
-          <!-- <li>
-            <a href="privacy_policy.php" class="footer-bottom-link">Privacy Policy</a>
-          </li> -->
-          <li>
-            <a href="sitemap_index.xml" class="footer-bottom-link">Sitemap</a>
-          </li>
-          <li>
-            <a href="terms_and_conditions.php" class="footer-bottom-link">T&C</a>
-          </li>
-          <li style="color: white;">
-            <div style="display: flex; align-items: center;">
-                <span class="material-symbols-outlined" style="margin-right: 5px;">language</span>
-                English
-            </div>
-          </li>
-          <li style="color: white;">
-            <div style="display: flex; align-items: center;">
-                <span class="material-symbols-outlined" style="margin-right: 5px;">currency_rupee
-                </span>
-                INR
-            </div>
-          </li>
-          <li class="footer-items">
-            <ion-icon name="logo-facebook"></ion-icon>
-          </li>
-          <li class="footer-items">
-          <a href="https://x.com/@the_application" style="text-decoration:none;"><ion-icon name="logo-twitter"></ion-icon></a>  
-          </li>
-          <li class="footer-items">
-            <ion-icon name="logo-instagram"></ion-icon>
-          </li>
 
+    <button class="btn btn-secondary">Contact Us !</button>
+
+  </div>
+</section>
+<meta name="description"
+  content="Welcome to the Class XII Admission Portal, your gateway to streamlined admissions in West Bengal. Discover top-rated higher secondary schools, simplify your application process, and secure your spot in the best institutions. Explore our user-friendly platform for hassle-free admissions today.">
+<footer class="footer">
+  <div class="footer-top">
+    <div class="container">
+      <div class="footer-brand">
+        <a href="#" class="logo">
+          <img src="../Assets/images/favicon.png" alt="Haggle logo" style="width: 15%;">
+        </a>
+        <p class="footer-text" description="Class XII Admission Portal">
+          Welcome to the Common Higher Secondary Admission Portal for students in West Bengal. Our platform simplifies
+          the application process for higher secondary schools across the state. Use our user-friendly search feature
+          to
+          find and apply to your preferred schools. Simply search for nearby schools and click 'Apply' to submit your
+          application. Our portal ensures a smooth and efficient admission experience, helping you secure a spot in
+          the
+          best higher secondary institutions in West Bengal.
+        </p>
+      </div>
+      <div class="footer-contact">
+        <h4 class="contact-title">Contact Us</h4>
+        <p class="contact-text">
+          Feel free to contact us for any inquiries or assistance.
+        </p>
+        <ul>
+          <li class="contact-item">
+            <ion-icon name="call-outline"></ion-icon>
+            <a href="tel:+919475755847" class="contact-link">+91 9475755847</a>
+          </li>
+          <li class="contact-item">
+            <ion-icon name="mail-outline"></ion-icon>
+            <a href="mailto:suchibratapatra2003@gmail.com" class="contact-link">suchibratapatra2003@gmail.com</a>
+          </li>
+          <li class="contact-item">
+            <ion-icon name="location-outline"></ion-icon>
+            <address>Diamond Harbour,West Bengal,INDIA</address>
+          </li>
+        </ul>
+        <br>
+        <ul>
+          <p>Quick Links</p>
+          <hr style="width: 30%; height: 2.5px; border: none; background-color:#3b79c9;">
+          <br>
+          <li class="contact-item">
+            <a href="index.php" class="contact-link">Home</a>
+          </li>
+          <li class="contact-item">
+            <a href="../../privacy_policy.php" class="contact-link">Privacy Policy</a>
+          </li>
+          <li class="contact-item">
+            <a href="../../Complete_List.php" class="contact-link">Search Results</a>
+          </li>
+          <li class="contact-item">
+            <a href="../../terms_and_conditions.php" class="contact-link">Terms & Conditions</a>
+          </li>
+          <li class="contact-item">
+            <a href="../../Digital-Fingerprint-Decoder.php" class="contact-link">Fingerprint Decoder</a>
+          </li>
+          <li class="contact-item">
+            <a href="../../password_guidelines.php" class="contact-link">Password Guidelines</a>
+          </li>
         </ul>
       </div>
+      <div class="footer-form">
+        <p class="form-text">
+          Subscribe to our newsletter for updates and news.
+        </p>
+        <form action="" class="form-wrapper">
+          <input type="email" name="email" class="input-field" placeholder="Enter Your Email" required>
+          <button type="submit" class="btn btn-secondary">Subscribe</button>
+        </form>
+      </div>
     </div>
-  </footer>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <ul class="copyright">
+        &copy; 2024 <a href="">Theapplication.in</a> All rights reserved
+      </ul>
+      <ul class="footer-bottom-list">
+        <!-- <li>
+            <a href="privacy_policy.php" class="footer-bottom-link">Privacy Policy</a>
+          </li> -->
+        <li>
+          <a href="sitemap_index.xml" class="footer-bottom-link">Sitemap</a>
+        </li>
+        <li>
+          <a href="terms_and_conditions.php" class="footer-bottom-link">T&C</a>
+        </li>
+        <li style="color: white;">
+          <div style="display: flex; align-items: center;">
+            <span class="material-symbols-outlined" style="margin-right: 5px;">language</span>
+            English
+          </div>
+        </li>
+        <li style="color: white;">
+          <div style="display: flex; align-items: center;">
+            <span class="material-symbols-outlined" style="margin-right: 5px;">currency_rupee
+            </span>
+            INR
+          </div>
+        </li>
+        <li class="footer-items">
+          <ion-icon name="logo-facebook"></ion-icon>
+        </li>
+        <li class="footer-items">
+          <a href="https://x.com/@the_application" style="text-decoration:none;"><ion-icon
+              name="logo-twitter"></ion-icon></a>
+        </li>
+        <li class="footer-items">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</footer>
