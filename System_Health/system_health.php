dcca94b0662bfd3cff52f8596f186860acd6a012
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-11-23 05:59:41 +0530

    Update system_health.php

--- System_Health/system_health.php
+++ System_Health/system_health.php
@@ -81,7 +81,7 @@ echo "<!DOCTYPE html>
         .file-name {
             flex-grow: 1;
             font-weight:200px;
-            font-size:20px;
+            font-size:15px;
         }
         .status-indicator {
             display: flex;
