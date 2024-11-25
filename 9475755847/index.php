f585a3b7e99bc8462e92f8954a97543aa04abd71
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-11-25 22:45:14 +0530

    Update index.php

--- 9475755847/index.php
+++ 9475755847/index.php
@@ -1,12 +1,11 @@
-<?php include('../favicon.php') ?>
 <!DOCTYPE html>
+<?php include('../favicon.php') ?>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../../Assets/css/index.css">
+  <link rel="preload" href="../../../Assets/css/index.css">
   <title>DHBSSPV</title>
 
   <!-- 
@@ -20,9 +19,9 @@
   <!-- 
     - google font link
   -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+  <link rel="preload" href="https://fonts.googleapis.com">
+  <link rel="preload" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preload" href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
   <!-- 
     - preload images
