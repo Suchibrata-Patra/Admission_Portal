2cb3cbcd3bf44a7fca484d5e99a9fae3b983c313
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-11-26 14:32:55 +0530

    Update database.php

--- 9475755847/database.php
+++ 9475755847/database.php
@@ -42,7 +42,7 @@ function loadEnv($file) {
 }
 
 loadEnv(dirname(__DIR__) . '/.env');
-$host = getenv("DB_HOST");
+$host = getenv('DB_HOST');
 $user = getenv('DB_USER');
 $password = getenv("DB_PASSWORD");
 $dbname = getenv('DB_NAME');
