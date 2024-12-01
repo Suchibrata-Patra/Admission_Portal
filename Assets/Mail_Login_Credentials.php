ca2bc02ab33399dd74650ff71c2d2d34cc55d12e
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-01 23:54:35 +0530

    Modified

--- Assets/Mail_Login_Credentials.php
+++ Assets/Mail_Login_Credentials.php
@@ -1,6 +1,6 @@
 <?php
 $mailid = 'otpverifier.2023@gmail.com';
-$mailid_login_password = 'cfbu phba ywgh ufkf';
+$mailid_login_password = 'ovkc rxxy auno vfpi';
 echo $mailid;
 echo $mailid_login_password;
 ?>
