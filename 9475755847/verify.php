2fb0a902a42028e89f1f39e5ed087664981a469a
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-02 01:19:50 +0530

    Update verify.php

--- 9475755847/verify.php
+++ 9475755847/verify.php
@@ -54,6 +54,7 @@ if (isset($_POST['edit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Email Verification</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
         body {
             background-color: #f7f7f7; /* Apple-style soft gray background */
