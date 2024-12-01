cb366a30060afd15b5522e762436f3bafb744408
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-01 22:00:31 +0530

    Update welcome.php

--- 9475755847/welcome.php
+++ 9475755847/welcome.php
@@ -94,7 +94,13 @@ if ($user['numberVerify'] == 0) {
       </div>
     </div>
   </div>
-  
+  <script>
+    // Hide preloader after 3 seconds
+    setTimeout(function () {
+      var preloader = document.getElementById('preloader');
+      preloader.style.display = 'none';
+    }, 4000); // 3000 milliseconds = 3 seconds
+  </script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 </body>
